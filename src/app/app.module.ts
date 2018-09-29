c9fbe89d38593adcfbd8b19bf7b7666f298bc39d
Kevin Stradtman <stradtkt@gmail.com>
2018-09-29 12:29:32 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { TimeComponent } from './components/time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    NotesComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule
